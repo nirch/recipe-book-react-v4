4e2612593d0cc8b9eae2f0a54395134729154829
nirch <nirh2@yahoo.com>
2021-01-07 10:35:23 +0200

    redirecting back to Home from Recipes if no activeUser

--- src/pages/RecipesPage/RecipesPage.js
+++ src/pages/RecipesPage/RecipesPage.js
@@ -1,7 +1,12 @@
+import { Redirect } from "react-router-dom";
 import RecipeNavbr from "../../components/RecipeNavbar/RecipeNavbr";
 
 function RecipesPage(props) {
     const {activeUser, onLogout} = props;
+
+    if (!activeUser) {
+        return <Redirect to="/"/>
+    }
 
     return (
         <div className="p-recipes">
