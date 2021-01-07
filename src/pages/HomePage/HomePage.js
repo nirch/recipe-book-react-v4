e4bbbdb7a3c63f3c8d1f4a25122c3bdf5e78821e
nirch <nirh2@yahoo.com>
2021-01-07 09:59:08 +0200

    adding bootstrap + initial navbar

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,10 @@
+import RecipeNavbr from "../../components/RecipeNavbar/RecipeNavbr";
 
 function HomePage() {
 
     return (
         <div className="p-home">
+            <RecipeNavbr/>
             Home Page
         </div>
     )
