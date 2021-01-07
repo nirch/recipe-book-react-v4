9f309bca44acd54c7c720d2a8f40a37f3b80c4ab
nirch <nirh2@yahoo.com>
2021-01-07 11:05:27 +0200

    Login page design

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,31 @@
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './LoginPage.css'
 
 function LoginPage() {
 
     return (
         <div className="p-login">
-            Login Page
+            <h1>Login to Recipe Book</h1>
+            <p>or <Link to="/signup">create an account</Link></p>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+               
+                <Button variant="success" type="submit" block>
+                    Login
+                </Button>
+            </Form>
         </div>
     )
 
