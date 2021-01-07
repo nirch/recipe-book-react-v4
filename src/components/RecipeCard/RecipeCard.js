0169b8b784ca9b869ee1677fe12e4e756714ce71
nirch <nirh2@yahoo.com>
2021-01-07 13:19:36 +0200

    RecipeCard design + cropping image to cover

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,10 +1,18 @@
+import { Card } from "react-bootstrap";
+import './RecipeCard.css'
 
 
 function RecipeCard(props) {
-    const {recipe} = props;
+    const { recipe } = props;
     return (
-        <div>
-            {recipe.name}
+        <div className="c-recipe-card">
+            <Card>
+                <Card.Img variant="top" src={recipe.img} />
+                <Card.Body>
+                    <Card.Title>{recipe.name}</Card.Title>
+                    <Card.Text>{recipe.desc}</Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
