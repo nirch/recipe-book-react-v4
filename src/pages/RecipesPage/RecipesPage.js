c53e776b7191e70d6460119affac7779d7a766fd
nirch <nirh2@yahoo.com>
2021-01-07 13:41:49 +0200

    new recipe button opens a Modal window

--- src/pages/RecipesPage/RecipesPage.js
+++ src/pages/RecipesPage/RecipesPage.js
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
+import NewRecipeModal from "../../components/NewRecipeModal/NewRecipeModal";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
 import RecipeNavbr from "../../components/RecipeNavbar/RecipeNavbr";
 import './RecipesPage.css'
 
 function RecipesPage(props) {
     const {activeUser, onLogout, recipes} = props;
+    const [showModal, setShowModal] = useState(false);
 
     if (!activeUser) {
         return <Redirect to="/"/>
     }
 
-    console.log(recipes);
+
+
     const recipesView = recipes.map(recipe => <Col key={recipe.id} lg={3} md={6}><RecipeCard recipe={recipe}/></Col>)
 
     return (
@@ -20,12 +24,13 @@ function RecipesPage(props) {
             <Container>
                 <div className="heading">
                     <h1>{activeUser.fname}'s Recipes</h1>
-                    <Button variant="link">New Recipe</Button>
+                    <Button variant="link" onClick={() => setShowModal(true)}>New Recipe</Button>
                 </div>
                 <Row>
                     {recipesView}
                 </Row>
             </Container>
+            <NewRecipeModal show={showModal} handleClose={() => setShowModal(false)}/>
         </div>
     )
 
