917f86b259fd2897b3e3ad461395bfa3b475919a
nirch <nirh2@yahoo.com>
2021-01-07 13:32:32 +0200

    adding new recipe button

--- src/pages/RecipesPage/RecipesPage.js
+++ src/pages/RecipesPage/RecipesPage.js
@@ -1,7 +1,8 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
 import RecipeNavbr from "../../components/RecipeNavbar/RecipeNavbr";
+import './RecipesPage.css'
 
 function RecipesPage(props) {
     const {activeUser, onLogout, recipes} = props;
@@ -17,7 +18,10 @@ function RecipesPage(props) {
         <div className="p-recipes">
             <RecipeNavbr activeUser={activeUser} onLogout={onLogout}/>
             <Container>
-                <h1>{activeUser.fname}'s Recipes</h1>
+                <div className="heading">
+                    <h1>{activeUser.fname}'s Recipes</h1>
+                    <Button variant="link">New Recipe</Button>
+                </div>
                 <Row>
                     {recipesView}
                 </Row>
