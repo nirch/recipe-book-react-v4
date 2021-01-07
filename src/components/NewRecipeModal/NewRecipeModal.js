37670d01481a2a1cba1924b68d17f84d6366231a
nirch <nirh2@yahoo.com>
2021-01-07 13:54:36 +0200

    Controlled componenets in NewRecipeModal

--- src/components/NewRecipeModal/NewRecipeModal.js
+++ src/components/NewRecipeModal/NewRecipeModal.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { Button, Modal, Form, Col, Row } from "react-bootstrap";
 
 
 function NewRecipeModal(props) {
     const { show, handleClose } = props;
+    const [name, setName] = useState("");
+    const [desc, setDesc] = useState("");
+    const [imgURL, setImgURL] = useState("");
 
     return (
         <Modal show={show} onHide={handleClose} size="xl">
@@ -16,7 +20,7 @@ function NewRecipeModal(props) {
                             Recipe Name
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="text" placeholder="Recipe Name" />
+                            <Form.Control type="text" placeholder="Recipe Name" value={name} onChange={e => setName(e.target.value)} />
                         </Col>
                     </Form.Group>
 
@@ -25,7 +29,7 @@ function NewRecipeModal(props) {
                             Recipe Description
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="text" placeholder="Recipe Description" />
+                            <Form.Control type="text" placeholder="Recipe Description" value={desc} onChange={e => setDesc(e.target.value)}  />
                         </Col>
                     </Form.Group>
 
@@ -34,7 +38,7 @@ function NewRecipeModal(props) {
                             Recipe Image URL
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="text" placeholder="Recipe Image URL" />
+                            <Form.Control type="text" placeholder="Recipe Image URL" value={imgURL} onChange={e => setImgURL(e.target.value)}  />
                         </Col>
                     </Form.Group>
                 </Form>
@@ -43,7 +47,7 @@ function NewRecipeModal(props) {
                 <Button variant="secondary" onClick={handleClose}>
                     Cancel
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" onClick={() => alert(name+desc+imgURL)}>
                     Create Recipe
                 </Button>
             </Modal.Footer>
