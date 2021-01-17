12ff0502ebfead76259248cafb6163f0a7882960
nirch <nirh2@yahoo.com>
2021-01-17 11:26:11 +0200

    installing charJS + rendering hard coded pie chart

--- src/pages/RecipesPage/RecipesPage.js
+++ src/pages/RecipesPage/RecipesPage.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { Pie } from "react-chartjs-2";
 import { Redirect } from "react-router-dom";
 import NewRecipeModal from "../../components/NewRecipeModal/NewRecipeModal";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
@@ -16,7 +17,33 @@ function RecipesPage(props) {
         return <Redirect to="/"/>
     }
 
-
+    const difficultyChartData = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+    }
+      
 
     const recipesView = recipes.map(recipe => <Col key={recipe.id} lg={3} md={6}><RecipeCard recipe={recipe}/></Col>)
 
@@ -31,6 +58,7 @@ function RecipesPage(props) {
                 <Row>
                     {recipesView}
                 </Row>
+                <Pie data={difficultyChartData}/>
             </Container>
             <NewRecipeModal show={showModal} handleClose={() => setShowModal(false)} addRecipe={addRecipe}/>
         </div>
